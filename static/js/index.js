82ade075798859defc1c263e23fb6c62c59df21c
Lakshmi <lakshmimohan2998@gmail.com>
2024-01-09 14:29:20 +0530

    done the webiste

---
+++ static/js/index.js
@@ -0,0 +1,32 @@
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
+// let online_items = document.querySelectorAll('.food_carousel .food_items ')
+
+// items.forEach((el) => {
+//     const minPerSlide = 5
+//     let next = el.nextElementSibling
+//     for (var i = 1; i < minPerSlide; i++) {
+//         if (!next) {
+//             // wrap carousel by using first child
+//             next = items[0]
+//         }
+//         let cloneChild = next.cloneNode(true)
+//         el.appendChild(cloneChild.children[0])
+//         next = next.nextElementSibling
+//     }
+// })
---
+++ test.py
@@ -0,0 +1,22 @@
+temp = input()
+
+# count = len(temp)
+# result = ''
+
+result_temp = sorted(temp,reverse=True)
+if temp == result_temp:
+  print("Its Palindrome")
+else:
+  print("Not palindrome")
+
+# for x in temp:
+#   print(x)
+#   result = result + temp[count]
+#   count = count - 1
+  
+# if  result == temp:
+#   print("Its Palindrome")  
+# else:
+#   print("Not Palindrome")
+  
+  
